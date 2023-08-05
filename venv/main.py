d74fefed91e79fe17c05f5717ba72ec13dd60aac
Amid6002 <pikuza142@gmail.com>
2023-08-05 18:58:43 +0300

    фавиы

---
+++ venv/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+News = [{"title" : "fbtherh", "text": "sghyjruk"}]
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/news_detail/<int:id>")
+def news_detail(id):
+    title = News[id]["title"]
+    text = News[id]["text"]
+    return render_template("news_detail.html", title = title, text = text)
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
